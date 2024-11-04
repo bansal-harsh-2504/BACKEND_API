471ae61f993128c453fd8d93d4801e3864fd0b74
harsh <focusedstudytarget2022@gmail.com>
2024-11-04 10:20:55 +0530

    get request

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.json({ network: 102, messaging: 0, jobs: 0, notifications: 12 });
 });
 
+app.get("*", (req, res) => {
+  res.send("Invalid url");
+});
+
 app.listen(3000, (req, res) => {
-  console.log("Seerver is listening to port 3000");
+  console.log("Server is listening to port 3000");
 });
