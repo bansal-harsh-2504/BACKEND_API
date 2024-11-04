67dfe36001cbc388057977ee8e84e9fb51d2d2c6
harsh <focusedstudytarget2022@gmail.com>
2024-11-04 10:36:50 +0530

    deploying to vercel

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/api/", (req, res) => {
   res.json({ network: 102, messaging: 0, jobs: 0, notifications: 12 });
 });
 
