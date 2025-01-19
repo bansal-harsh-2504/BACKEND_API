fb04ea5de9c9f18fea0ac889626fa83c79d96cfb
harsh <focusedstudytarget2022@gmail.com>
2025-01-19 19:41:45 +0530

    new route for token launchpad

--- api/index.js
+++ api/index.js
@@ -2,8 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.get("/api/", (req, res) => {
+app.get("/api", (req, res) => {
   res.json({ network: 102, messaging: 0, jobs: 0, notifications: 12 });
+});
+
+app.get("/metadata", (req, res) => {
+  res.json({
+    mint: "BuMMZWi5s831uyd7rYJ9jD6doC8Fe4iZFpsJWUpcC6WW",
+    name: "Hulk",
+    symbol: "RAGE",
+    image: "https://res.cloudinary.com/dqhpdgamm/image/upload/v1736172900/uaqqfnd3t6scnyihp18s.jpg",
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("API WORKING");
 });
 
 app.get("*", (req, res) => {
